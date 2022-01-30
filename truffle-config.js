2abec233aa22e8c44a0c61c170ba0e9e3a1c5b32
Eiji Kuroda <e@96d.jp>
2022-01-31 07:15:22 +0900

    wip

--- truffle-config.js
+++ truffle-config.js
@@ -1,9 +1,17 @@
+const HDwalletProvider = require("truffle-hdwallet-provider");
 module.exports = {
   contracts_build_directory: "./client/src/contracts",
   networks: {
     development: {
       host: "172.22.112.1",
       port: 7545,
+      network_id: "*",
+    },
+    goerli: {
+      provider: () => {
+        const mnemonic = process.env["MNEMONIC"];
+        return new HDwalletProvider(mnemonic, "http://127.0.0.1:8545");
+      },
       network_id: "*",
     },
   },
